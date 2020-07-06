da073607013d7d271affe5871cc3c9d127d4e53d
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-07-06 12:23:04 -0600

    modificando calculadora

---
+++ Ejercicios/Ejercicio6-Calculadora/calculadora.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    	// Datos de entrada 
+	int a = 0;
+	int b = 0;
+	int resultado = 0;
+	
+	cout << "Ingrese el valor de a: ";
+	cin >> a;
+	
+	cout << endl;
+	
+	cout << "Ingrese el valor de b: ";
+	cin >> b;
+	
+	
+	//Proceso
+	resultado = a + b;
+     cout << endl;
+	cout << "Resultado de la suma es: " << resultado;
+    cout << endl;
+
+    resultado = a * b;
+    cout << endl;
+    cout << "Resultado de la multiplicacion es: " << resultado;
+	cout << endl;
+
+    resultado = a - b;
+    cout << endl;
+    cout << "Resultado de la resta es: " << resultado;
+    cout << endl;
+    
+    resultado = a / b;
+    cout << endl;
+    cout << "Resultado de la division es: " << resultado;
+	cout << endl;
+
+
+    return 0;
+}
+
+
