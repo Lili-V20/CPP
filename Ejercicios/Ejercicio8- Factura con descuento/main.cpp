3ded1041b565d25d40862c9276427813b2d76026
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-07-17 12:40:42 -0600

    agregando factura con descuento

---
+++ Ejercicios/Ejercicio8- Factura con descuento/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	    // Datos de entrada
+    double subtotal = 0 ;
+    double total = 0 ;
+    double impuesto = 0.15 ;
+    int descuento = 0;
+    double CalculoDescuento = 0;
+    double CalculoImpuesto = 0;
+
+    cout << " Ingrese el subtotal de la factura: " ;
+    cin >> subtotal;
+    
+    cout << "Ingrese el descuento (0, 10, 15, 20) ";
+    cin >> descuento;
+
+    // Proceso
+    CalculoDescuento = (subtotal * descuento) / 100;
+	CalculoImpuesto = (subtotal -  CalculoDescuento) * 0.15;
+    total = subtotal - CalculoDescuento + CalculoImpuesto;
+
+    // Salida
+	cout << endl;
+    cout << " Total a pagar es: " << total;
+	return 0;
+}
