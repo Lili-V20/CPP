3378c41974e384a8c9a0a1db41f94869a3362ebe
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-08-02 20:02:39 -0600

    Agregando Ejercicio de Funciones Enteras

---
+++ Ejercicios/Ejercicio29-Funciones Enteras/funcionInt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar(int a, int b){
+    return a + b;
+}
+
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+    cout << sumar(5, 7);
+    cout << endl;
+    cout << sumar(10, 7);
+    cout << endl;
+    cout << sumar(5, 18);
+    cout << endl;
+    cout << sumar(1, 7);
+    cout << endl;
+    return 0;
+}
