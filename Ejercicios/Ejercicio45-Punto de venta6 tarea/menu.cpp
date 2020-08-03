76fb983cd0cb1146ce25fa0fbd0c396163ecbe22
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-08-03 15:35:02 -0600

    tarea

---
+++ Ejercicios/Ejercicio45-Punto de venta6 tarea/menu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+extern void productos(int opcion);
+extern void imprimirFactura();
+
+void menu()
+{
+
+    int opcion = 0;
+
+    while (true)
+    {
+        cout << "*****";
+        cout << "MENU";
+        cout << "*****";
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        system ("cls");
+        
+        cout << "1 - BEBIDAS CALIENTES" << endl;
+        cout << "2 - BEBIDAS FRIAS" << endl;
+        cout << "3 - REPOSTERIA" << endl;
+        cout << "4 - IMPRIMIR FACTURA" << endl;
+        cout << "0 - SALIR" << endl;
+
+
+        cout << endl;
+        cout << "Ingrese una opcion:  ";
+        cin >> opcion;
+
+        if (opcion == 0)
+        {
+            break;
+        }
+        if (opcion == 4)
+        {
+            imprimirFactura();
+        }else{
+        productos (opcion);    
+        }
+        
+    }
+
+}
