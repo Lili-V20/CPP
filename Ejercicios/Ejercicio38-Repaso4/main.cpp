c3131005ea538553f3b574f25cd77b6dcea2e147
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-08-03 11:15:21 -0600

    agregando repaso 4

---
+++ Ejercicios/Ejercicio38-Repaso4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+    
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
