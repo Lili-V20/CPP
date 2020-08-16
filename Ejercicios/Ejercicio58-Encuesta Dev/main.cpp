dc90a0908be84d6535bcf33211d6803080de0bb3
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-08-16 16:40:46 -0600

    Agregando ejercicio 58 con visual

---
+++ Ejercicios/Ejercicio58-Encuesta Dev/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	    string alumnos[3];
+    string encuesta [3][2] =
+    {
+        {"Llevara clases el siguiente periodo?", ""},
+        {"Matriculara L2?", ""},
+        {"Matriculara L con el Lic. Billy?", ""}
+    };
+    int respuestas[3] = {0, 0, 0};
+
+    cout << "Conteste la encuesta con s o n" << endl;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Ingrese su nombre: ";
+        cin >> alumnos[i];
+        cout << endl;
+
+        for (int j = 0; j < 3; j++)
+        {
+            cout << encuesta[j][0] << ": ";
+            cin >> encuesta[j][1];
+
+            if (encuesta[j][1] == "s"){
+                respuestas[j]++;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Resumen" << endl;
+    cout << "Respuesta pregunta 1: " << respuestas[0] << endl;
+    cout << "Respuesta pregunta 2: " << respuestas[1] << endl;
+    cout << "Respuesta pregunta 3: " << respuestas[2] << endl;
+	return 0;
+}
