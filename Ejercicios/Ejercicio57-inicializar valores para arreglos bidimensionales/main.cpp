de168b9ed2343a7724751f6a9bdf60d9f76fbff9
Lili Velasquez <velasquez.lili1999@gmail.com>
2020-08-16 11:21:43 -0600

    .

--- Ejercicios/Ejercicio57-inicializar valores para arreglos bidimensionales/main.cpp
+++ Ejercicios/Ejercicio57-inicializar valores para arreglos bidimensionales/main.cpp
@@ -4,14 +4,26 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int filas = 3;
+    // string nombres[3][2];
+
+    // nombres[0][0] = "Juan";
+    // nombres[0][1] = "Perez";
+    
+    // nombres[1][0] = "Maria";
+    // nombres[1][1] = "Martinez";
+
+    // nombres[2][0] = "Pedro";
+    // nombres[2][1] = "Jimenez";
+
+    int filas = 4;
     int columnas = 2;
 
-    string nombres[filas][columnas] =
+    string nombres[filas][columnas] = 
     {
-        {"Juan", "Perez"},
-        {"Maria", "Martinez"},
-        {"Pedro", "Jimenez"}
+        { "Juan", "Perez" },
+        { "Maria", "Martinez" },
+        { "Pedro", "Jimenez" },
+        { "Pablo", "Hernandez" }
     };
 
     for (int i = 0; i < filas; i++)
@@ -23,6 +35,7 @@ int main(int argc, char const *argv[])
         
         cout << endl;
     }
+    
 
     return 0;
 }
